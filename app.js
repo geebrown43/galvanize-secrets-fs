385ffff6cf06c7e856de6d6a40298958f1aa7a40
Ellis Mwekunda <Scareb15@gmail.com>
2017-10-19 10:16:34 -0600

    changed from get to use

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-app.get('/', (req, res, next) => {
+app.use('/', (req, res, next) => {
 	res.render('index')
 })
 
